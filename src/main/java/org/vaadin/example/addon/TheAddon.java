f6f06e48883a07db59e88c791c33269dd37e3734
Mario David <mario.david@blacklane.com>
2022-07-24 16:05:20 +0200

    Initial commit

---
+++ src/main/java/org/vaadin/example/addon/TheAddon.java
@@ -0,0 +1,10 @@
+package org.vaadin.example.addon;
+
+import com.vaadin.flow.component.html.Div;
+
+public class TheAddon extends Div {
+
+    public TheAddon() {
+        setText("Hello");
+    }
+}
